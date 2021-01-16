6af10c7aeb902c6ca1961c0a0746accce0fab574
rikkapro0128 <stkssc002@gmail.com>
2021-01-17 06:56:41 +0700

    save code show menu profile

---
+++ public/handleView/click.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Handle hide user profile if user click different area
+    $(document).click(function(event) {
+        //Hide the menus if visible
+        if($(event.target).closest('.manager ._user').length && !$('.manager ._user').hasClass('show')) {
+            $('.manager ._user .--dropdown').show();
+            $('.manager ._user').addClass('show');
+        }else {
+            $('.manager ._user .--dropdown').hide();    
+            $('.manager ._user').removeClass('show');
+        }
+        if($(event.target).closest('.--dropdown').length) {
+            $('.manager ._user .--dropdown').show();
+        }
+        
+    });
+        
+});
