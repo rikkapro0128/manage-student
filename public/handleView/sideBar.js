4b8867961c2f857119b8d3cdb24a0f5aaccaad70
NguyenTanPhap <stkssc002@gmail.com>
2021-01-23 02:50:22 +0700

    complete sideShow 80%

--- public/handleView/sideBar.js
+++ public/handleView/sideBar.js
@@ -2,41 +2,92 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let count = 0;
     let position = 0; 
-    let isDrag = false;
     let isDrop = false;
+    let isHold = false;
+    let isDrag = false;
+    let point = 0;
+    let speed = 20;
+    let coordinate = {
+        mouseX: 0,
+        mouseY: 0,
+    }
 
     class side {
-        
+        leftOrRight(entry, present, element) {
+            // console.log(entry, ' ', present) 
+            if(entry > present) {
+                point += -speed;
+                if(point <= -760) {
+                    point = -760;
+                }
+                $(element).css({    
+                    // translate to left
+                    transform: `translate3d(${point}px, 0px, 0px)`,
+                    transition: ``, 
+                    // you have () when write event.pageX - coordinate.mouseX
+                })
+            }
+            else {
+                point += speed;
+                if(point >= 0) {
+                    point = 0;
+                }
+                $(element).css({
+                    // translate to right
+                    transform: `translate3d(${point}px, 0px, 0px)`,
+                    transition: ``, 
+                    // you have () when write event.pageX - coordinate.mouseX
+                })
+            }
+            console.log(point)  
+            
+        }
         hold(event) {
-            $(this).on('mousemove', sideShow.drag);
+            isHold = true;
+            isDrop = false;
+            isDrag = true;
+            coordinate.mouseX = event.pageX;
+            coordinate.mouseY = event.pageY;
         }
         drag(event) {
-            console.log('draging!')
-            console.log('X: ' + event.pageX + 'Y: ' + event.pageY)
+            if(!isDrop && isHold) {
+                sideShow.leftOrRight(coordinate.mouseX, event.pageX, this)
+                if(isDrag) {
+                    $(this).find('a').on('click', function(event) {
+                        event.preventDefault();
+                    })
+                }
+            }
         }
         drop(event) {
-            console.log('remove mousedown')
-            $(this).off('mousemove')
+            isDrop = true;
+            isHold = true;
+            isDrag = false;
         }
         
     }
     
-    let sideShow = new side();
+    const sideShow = new side();
     
     $('.story_appoint').on('mousedown', sideShow.hold);
     $('.story_appoint').on('mouseup', sideShow.drop);
+    $('.story_appoint').on('mousemove', sideShow.drag);
 
-    setInterval(function() {
-        position += 190;
-        count += 1;
-        if(count < 5) {
-            $('.story_appoint').css('transform', `translate3d(-${position}px, 0px, 0px)`)
-        }else {
-            position = 0;
-            count = 0;
-        }
-    }, 4000)
+    if(!isDrag) {
+        setInterval(function() {
+            console.log(isDrag)
+            if(count < 5) {
+                $('.story_appoint').css({
+                    transform: `translate3d(-${position}px, 0px, 0px)`,
+                    transition: `all 800ms ease-in-out`,
+                })
+                position += 190;
+                count += 1;
+            }else {
+                position = 0;
+                count = 0;
+            }
+        }, 4000)
+    }
     
-    
-
 });
